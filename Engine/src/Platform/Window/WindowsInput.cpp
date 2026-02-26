4647085abfe5b2449bfadcc7410d2659ef0f9384
PriyanshThapliyal <priyanshthapliyal0@gmail.com>
2026-02-26 18:23:07 +0530

    Added static input wrapper

---
+++ Engine/src/Platform/Window/WindowsInput.cpp
@@ -0,0 +1,35 @@
+#include <enpch.h>
+#include "WindowsInput.h"
+#include "Engine/Application.h"
+#include <GLFW/glfw3.h>
+
+namespace Engine
+{
+	Input* Input::s_Instance = new WindowsInput(); // Initialize the static instance of Input
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return { (float)xpos, (float)ypos };
+	}
+
+}
--- SandBox/src/SandBox.cpp
+++ SandBox/src/SandBox.cpp
@@ -17,12 +17,16 @@ public:
 protected:
 	virtual void OnUpdate(float deltaTime) override
 	{
-		EN_CORE_TRACE("Delta Time -> {0} ", deltaTime);
-		
+		//EN_CORE_TRACE("Delta Time -> {0} ", deltaTime);
+
 		if (Engine::Input::IsKeyPressed(Engine::Key::W))
 		{
 			EN_CORE_INFO("W key pressed");
 		}
+
+		EN_CORE_INFO("Mouse Position -> {0} , {1} ", Engine::Input::GetMousePosition().first, Engine::Input::GetMousePosition().second);
+		//EN_CORE_INFO("Mouse Button Pressed -> {0}", Engine::Input::IsMouseButtonPressed(0)); 
+
 		// Update game logic here
 	}
 	virtual void OnRender() override
