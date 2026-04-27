53240218e73782ccd9f2923d416d8a58154bc747
PriyanshThapliyal <priyanshthapliyal0@gmail.com>
2026-04-27 21:49:59 +0530

    feat(components) : added 2 new component  and made some changes in SpriteComponent

---
+++ Engine/src/Scene/Components/EnemyComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Engine
+{
+	struct EnemyComponent
+	{
+		float Time = 0.0f;
+	};
+}
--- Engine/src/Scene/Components/SpriteRendererComponent.h
+++ Engine/src/Scene/Components/SpriteRendererComponent.h
@@ -6,8 +6,13 @@ namespace Engine
 {
 	struct SpriteRendererComponent
 	{
-		glm::vec4 Color = { 1.0f, 0.0f, 0.0f, 1.0f };
+		glm::vec4 Color = { 1.0f, 0.0f, 0.0f, 1.0f }; // tint
+		std::shared_ptr<Texture> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
+
+		SpriteRendererComponent(const glm::vec4& color)
+			: Color(color) {}
 	};
 }
